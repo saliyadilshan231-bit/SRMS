e2aeb256e3dff150573360f6c7950d9addb818ed
shehani11 <shehaniegodagedara@gmail.com>
2026-04-07 17:16:43 +0530

    just added a comment

--- app/timed-quiz.tsx
+++ app/timed-quiz.tsx
@@ -3,3 +3,5 @@ import TimedQuizModuleSelectScreen from '@/screens/TimedQuizModuleSelectScreen';
 export default function TimedQuizRoute() {
   return <TimedQuizModuleSelectScreen />;
 }
+
+//comment
